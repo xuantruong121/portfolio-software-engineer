e46f30b2c77300fd3ffdd040d12cdde38a43b8e4
xuantruong121 <luamoi2014@gmail.com>
2026-07-27 11:07:35 +0700

    chore: update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Configure base for GitHub Pages
+  base: '/portfolio-software-engineer/', // Configure base for GitHub Pages
 })
